2de749a7c319cdcd1eaaff61fa77a736ae7a76a7
Gabriel010302 <91874872+Gabriel010302@users.noreply.github.com>
2023-05-10 00:09:50 +0700

    Update Pelanggan.php

--- webnative/admin/models/Pelanggan.php
+++ webnative/admin/models/Pelanggan.php
@@ -6,12 +6,30 @@ class Pelanggan {
         $this->koneksi = $dbh;
     }
     public function dataPelanggan(){
-        $sql = "SELECT * FROM pelanggan";
+        $sql = "SELECT pelanggan.*, kartu.nama as kartu FROM pelanggan INNER JOIN
+        kartu ON kartu.id = pelanggan.kartu_id ";
         $ps = $this->koneksi->prepare($sql);
         $ps->execute();
         $rs = $ps->fetchAll();
         return $rs;
     }
+
+    public function getPelanggan($id){
+        $sql = "SELECT pelanggan.*, kartu.nama as kartu FROM pelanggan INNER JOIN
+        kartu ON kartu.id = pelanggan.kartu_id WHERE pelanggan.id = ? ";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute([$id]);
+        $rs = $ps->fetch();
+        return $rs;
+    }
+    public function simpan($data){
+        $sql = "INSERT INTO pelanggan(kode, nama_pelanggan, jk, tmp_lahir, tgl_lahir, email, kartu_id)
+        VALUES (?,?,?,?,?,?,?)";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute($data);
+        
+    }
 }
+
 
 ?>
